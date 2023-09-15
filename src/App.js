76e46c93c0148212ddd34cdad004adb9094cbb2f
edesi0 <91793575+edesi0@users.noreply.github.com>
2023-09-15 17:29:33 -0300

    adicionando os novos arquivos

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Logo from './componentes/logo/index.js';
+
+function App() {
+  return (
+    <div classname="App">
+      <header classname="App-header">
+      <Logo></Logo>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/logo/index.js
@@ -0,0 +1,18 @@
+import logo from '../../img/logo.svg';
+import './estilo.css';
+
+function Logo() {
+
+    return (
+        <div className='logo'>
+
+               <img className='logo-img'
+                src={logo}
+                alt='Logo Alura'></img>
+                
+           <p> <strong>Alura Books</strong></p>
+        </div>
+    );
+
+}
+export default Logo;
