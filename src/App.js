c3850f81956d99eb2f92db19b5ccaec7cd6db374
edesi0 <91793575+edesi0@users.noreply.github.com>
2023-09-16 09:52:11 -0300

    criando as lista de icones

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
+import listaCabecalho from './componentes/listaCabecalho/index';
 import Logo from './componentes/logo/index.js';
+
 
 function App() {
   return (
     <div classname="App">
       <header classname="App-header">
       <Logo></Logo>
-      <ul className='opcoes'>
-        <li className='opcao'>CATEGORIAS</li>
-        <li className='opcao'>MINHA ESTANTE</li>
-        <li className='opcao'>FAVORITOS</li>
-      </ul>
+      <listaCabecalho></listaCabecalho>
       </header>
     </div>
   );
---
+++ src/componentes/listaCabecalhoIcones/index.js
@@ -0,0 +1,14 @@
+import perfil from '../../img/perfil.svg';
+import sacola from '../../img/sacola.svg';
+
+const listaIcones=[perfil,sacola];
+
+function listaCabecalhoIcones(){
+    return(
+        <ul>
+
+        </ul>
+    );
+}
+
+ export default listaCabecalhoIcones;
